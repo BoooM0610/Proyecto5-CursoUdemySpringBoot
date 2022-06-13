39170aa083f388eda201256fa9c9944356b1aba1
BoooM0610 <eduardomartinezoc@gmail.com>
2022-06-13 09:11:06 +0200

    Initial commit

---
+++ src/main/java/com/example/proyecto5error/Proyecto5CursoUdemySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto5error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto5CursoUdemySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto5CursoUdemySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto5error/Proyecto5CursoUdemySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto5error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto5CursoUdemySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
